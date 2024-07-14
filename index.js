144a7db569ed4510508e49a3cd49183230b326d5
Mujahid2000 <your.email@example.com>
2024-07-14 23:36:19 +0600

    add some code jwt

--- index.js
+++ index.js
@@ -46,13 +46,15 @@ async function run() {
     
 
     
-    app.post('/jwt', async(req, res) =>{
-      const user = req.body;
-      // console.log(user);
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '24h'})
-      // console.log(process.env.ACCESS_TOKEN_SECRET);
-      res.send({token})
-    })
+    app.post('/jwt', async (req, res) => {
+      try {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '24h' });
+        res.send({ token });
+      } catch (error) {
+        res.status(500).send({ message: 'Error generating token', error });
+      }
+    });
 
     const verifyToken = (req, res, next) => {
       const token = req.headers.authorization;
