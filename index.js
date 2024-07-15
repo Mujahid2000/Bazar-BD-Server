6bdf156eb733a96994cd2c1056e0fb6c5e74ecaa
Mujahid2000 <your.email@example.com>
2024-07-15 19:36:07 +0600

    add some new resueble code

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ async function run() {
     app.post('/jwt', async (req, res) => {
       try {
         const user = req.body;
-        
+        console.log(user);
         const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '24h' });
         res.send({ token });
       } catch (error) {
@@ -60,6 +60,7 @@ async function run() {
     
     const verifyToken = (req, res, next) => {
       const token = req.headers.authorization;
+      console.log(token);
       if (!token) {
         return res.status(401).json({ message: 'Unauthorized: No token provided' });
       }
